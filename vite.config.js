0d4478da3132f104bc089fc8553499aec824d31c
linzekai <linzekai@medsimmeta.com>
2026-02-28 11:29:20 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,32 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-import Components from 'unplugin-vue-components/vite';
-import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
-    server: {
-        allowedHosts: ['lzk.medsimverse.com'],
-        port: 35684,
-        proxy: {
-            '/api': {
-                target: 'http://browser-plug-api.yangjibao.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-                secure: false
-            }
-        },
+  base: "yjj",
+  server: {
+    allowedHosts: ["lzk.medsimverse.com"],
+    port: 35684,
+    proxy: {
+      "/api": {
+        target: "http://browser-plug-api.yangjibao.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
     },
-    plugins: [
-        vue(),
-        tailwindcss(),
-        Components({
-            resolvers: [
-                AntDesignVueResolver({
-                    importStyle: false,
-                }),
-            ],
-        }),],
-})
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false,
+        }),
+      ],
+    }),
+  ],
+});
